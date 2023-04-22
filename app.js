8577ea565242f0e2405e768b7ad959092e479755
Olga1Chernova <107894331+Olga1Chernova@users.noreply.github.com>
2023-04-23 01:48:52 +0300

    Update app.js

--- app.js
+++ app.js
@@ -2,16 +2,18 @@ const express = require('express')
 const logger = require('morgan')
 const cors = require('cors')
 require('dotenv').config();
-const contactsRouter = require('./routes/api/contacts-routes')
+const contactsRouter = require('./routes/api/contacts-routes');
+const authRouter = require("./routes/api/auth-routes");
 
 const app = express();
 
-
-const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
+const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short';
 
 app.use(logger(formatsLogger))
 app.use(cors())
 app.use(express.json())
+
+app.use('/api/auth', authRouter)
 
 app.use('/api/contacts', contactsRouter)
 
